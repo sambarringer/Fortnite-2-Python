2939b1ac0e4a8c0cc5d781eb01aeb24b83433c67
sambarringer <43954766+sambarringer@users.noreply.github.com>
2018-10-22 15:51:56 +0100

    Update Fortnite 2 - Development - v0.1.py

--- Fortnite 2 - Development - v0.1.py
+++ Fortnite 2 - Development - v0.1.py
@@ -8,10 +8,9 @@ health = 100
 
 print("""
 Welcome,""", playerName ,"""to Fortnite 2!
-
 Make sure to default dance on the haters!""")
 
-#Define this and use it as a function for else if none of the options are selected when the user inputs
+
 def runMenu():
     mainMenu = input("""Main Menu:
 Solo
@@ -19,40 +18,38 @@ Duos
 Squad
 Settings
 Skins
-
 Type in the one you want to use: """)
 
-if mainMenu == "Solo":
-    print("Selected solo!")
-    time.sleep(2)
-    os.system('cls')
-    print("Fortnite 2 is loading.")
-    time.sleep(2)
-    os.system('cls')
-    print("Fornite 2 is loading..")
-    time.sleep(2)
-    os.system('cls')
-    print("Fortnite 2 is loading...")
-    time.sleep(2)
-    os.system('cls')
-    input("Do you want to thank the battle bus driver? Y/N")
-    if input == "Y":
-        print("Thanks battle bus driver")
+    if mainMenu == "Solo":
+        print("Selected solo!")
+        time.sleep(2)
+        os.system('cls')
+        print("Fortnite 2 is loading.")
+        time.sleep(2)
+        os.system('cls')
+        print("Fornite 2 is loading..")
+        time.sleep(2)
+        os.system('cls')
+        print("Fortnite 2 is loading...")
+        time.sleep(2)
+        os.system('cls')
+        input("Do you want to thank the battle bus driver? Y/N: ")
+        if input == "Y":
+            print("Thanks battle bus driver")
+        else:
+            print("Wowie aren't you nice")
+
+
+
+    elif mainMenu == "Duos":
+        print("Cannot run right now")
+    elif mainMenu == "Squad":
+        print("Cannot run right now")
+    elif mainMenu == "Skins":
+        print("Thanos dududududududu Thanos dududududududuudud Thanos dududududududu")
+    elif mainMenu == "Settings":
+        input("""Settings:
+    """)
     else:
-        print("Wowie aren't you nice")
-
-
-
-elif mainMenu == "Duos":
-    print("Cannot run right now")
-elif mainMenu == "Squad":
-    print("Cannot run right now")
-elif mainMenu == "Skins":
-    print("Thanos dududududududu Thanos dududududududuudud Thanos dududududududu")
-elif mainMenu == "Settings":
-    input("""Settings:
-
-
-""")
-else:
-    runMenu()
+        runMenu()
+runMenu()
